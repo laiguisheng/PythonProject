4d500c071a28058b049dbbb15910a8e85f3f4798
laiguisheng <3044170061@qq.com>
2025-10-09 16:29:06 +0800

    1

--- 判断兔子种群数.py
+++ 判断兔子种群数.py
@@ -8,7 +8,7 @@ if __name__=='__main__':
         tu=tu1+tu2#迭代求出当前月份的兔子对数
         print('%6d'%tu,end="    ")#输出当前月份的兔子对数
         if i%4==0:
-            print()#每行输出4个
+            print()#每行输出4个，空print会默认输出一个换行符
         tu2=tu1#为下一次迭代做准备，求出新的tu2
         tu1=tu#求出新的tu1
         i=i+1
