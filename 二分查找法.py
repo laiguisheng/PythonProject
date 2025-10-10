778962866bdc2a7a585bc17d07f4b4fce5c6aa14
laiguisheng <3044170061@qq.com>
2025-10-10 21:36:45 +0800

    1

---
+++ 二分查找法.py
@@ -0,0 +1,25 @@
+if __name__=='__main__':
+    a=[1,2,3,4,45,56,567,765,8887,9999,97865]
+    low=0
+    n=len(a)
+    high=n-1
+    k=-1
+    print('a数组中的数据如下：')
+    for i in a:
+        print(i,end=' ')
+    print()
+    m=int(input('输入查询的数m=：'))
+    while low<=high:
+        mid=(low+high)//2
+        if m<a[mid]:
+            high=mid-1
+        else:
+            if m>a[mid]:
+                low=mid+1
+            else:
+                k=mid
+                break
+    if k>=0:
+        print("m=%d,序号4=%d"%(m,k))
+    else:#没找到k就不会被赋值，仍为一开始的负数
+        print('没找到')
---
+++ 选择排序.py
@@ -0,0 +1,23 @@
+def selectionSort(a):
+    #求出列表的长度
+    n=len(a)
+
+    for i in range(0,n-1):
+        for j in range(i+1,n):
+            #交换
+            if a[j] < a[i]:
+                t=a[i]
+                a[i]=a[j]
+                a[j]=t
+    for i in a:
+        print(i,end=' ')
+
+if __name__=='__main__':
+    print("请为列表元素赋值，列表末尾不能有空格：")
+    x=input()
+    a=x.split(" ")#以空格方式分割每个元素
+    for i in range(0,len(a)):#输入多个值
+        a[i]=int(a[i])
+    print("你输入的列表元素为：\n",a)
+    print('经过交换后的数组元素为：')
+    selectionSort(a)
